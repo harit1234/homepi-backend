30e1289228839bacedbd3a550014513060f17c5c
harit1234 <hrtmeans@gmail.com>
2018-09-25 15:39:24 +0530

    1st gpio api on channel/pin 7

--- config/gpio.js
+++ config/gpio.js
@@ -3,9 +3,9 @@ var gpio = require('rpi-gpio');
 // exports.setup() = gpio.setup()
 
 gpio.setup(7,'DIR_OUT',(err)=>{
-    return err.toString();
+    return err;
 })
 
 exports.defaulton = gpio.write(7,true,(err)=>{
-    return err.toString();
+    return err;
 })
--- routes/hc.js
+++ routes/hc.js
@@ -6,13 +6,12 @@ var bodyparser = require('body-parser');
 router.use(bodyparser.urlencoded({extended:true}))
 router.use(bodyparser.json())
 
+
 router.get('hcswitch/:id',(req,res)=>{
     param = req.params.id.substring(2);
     if(req.body.switch1 == true)
     {
-        res.json({
-            result : gpio.defaulton()
-        })
+        console.log(gpio.defaulton)
     }
 })
 module.exports = router
